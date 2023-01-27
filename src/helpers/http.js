85e748e8a482f0f22899ba1b3b1a51d2c76bec61
Lino Puma Ticona <nearlino20@gmail.com>
2023-01-27 08:47:08 -0500

    adsad

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const http = axios.create({
-  //baseURL: "https://plankton-app-848ak.ondigitalocean.app/api",
-  baseURL: "http://sistema-admision-back.test/api",
+  baseURL: "https://plankton-app-848ak.ondigitalocean.app/api",
+  //baseURL: "http://sistema-admision-back.test/api",
   
 });
 
